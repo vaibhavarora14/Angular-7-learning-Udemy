ca771892510fd3683ef4500d1ae743c94f24e8e3
varora1406 <varora1406@gmail.com>
2019-06-18 15:03:29 +0530

    - basic project for directives example

---
+++ directives-example/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
+}
--- shopping-recipe-course/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ shopping-recipe-course/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -8,18 +8,46 @@ import { empty } from 'rxjs';
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.scss']
 })
-export class ShoppingListEditComponent {
+export class ShoppingListEditComponent implements OnInit{
+  
   nameValue: string;
   amountValue: number;
+  addButtonState: boolean;
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+  @ViewChild('addButton', {static: true}) addButtonRef: ElementRef;
   @Output() ingredient = new EventEmitter<Ingredient>();
 
+  constructor() {
+    // this.validateForm();
+  }
+
+  ngOnInit(): void {
+    this.validateForm();
+  }
+
   addIngredient(): void {
-    const ingredient = new Ingredient(this.nameValue, this.amountValue);
-    this.ingredient.emit(ingredient);
-    this.clearForm();
+    if (this.nameInputRef.nativeElement.value && this.amountInputRef.nativeElement.value) {
+      const ingredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+      this.ingredient.emit(ingredient);
+      this.clearForm();
+    }
+    // const ingredient = new Ingredient(this.nameValue, this.amountValue);
+    // this.ingredient.emit(ingredient);
+    // this.clearForm();
+  }
+
+  validateForm() {
+    if (this.nameInputRef.nativeElement.value && this.amountInputRef.nativeElement.value) {
+      // this.addButtonRef.nativeElement.disabled = false;
+      this.addButtonState = false;
+    } else {
+      // this.addButtonRef.nativeElement.disabled = true;
+      this.addButtonState = true;
+    }
   }
 
   clearForm(): void {
-    this.nameValue = this.amountValue = undefined;
+    this.nameInputRef.nativeElement.value = this.amountInputRef.nativeElement.value = undefined;
   }
 }
