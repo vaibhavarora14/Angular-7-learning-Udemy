f529746433e5d51ee941cd44e1d85c2734fe00bf
Vaibhav Arora <varora@catalystone.com>
2019-06-25 15:14:15 +0530

    #1 submitting async pipe usage example

--- pipes-example/src/app/app.component.ts
+++ pipes-example/src/app/app.component.ts
@@ -35,6 +35,12 @@ export class AppComponent {
 
   filteredStatus: string = '';
 
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
