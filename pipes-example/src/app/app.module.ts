0962fc71526abfa8f35c9fa33c1dde4deff9d7fe
Vaibhav Arora <varora@catalystone.com>
2019-06-25 15:03:41 +0530

    - submitting filter pipe and it's usage

--- pipes-example/src/app/app.module.ts
+++ pipes-example/src/app/app.module.ts
@@ -6,20 +6,26 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShortenPipe,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatListModule,
     MatChipsModule,
+    MatInputModule,
     FlexLayoutModule,
   ],
   providers: [],
