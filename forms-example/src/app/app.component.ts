15a6addc90d378381f0cc589e0a58ebbdbd1831e
Vaibhav Arora <varora@catalystone.com>
2019-06-27 16:12:36 +0530

    #2 - submitting form state and radio button handling

--- forms-example/src/app/app.component.ts
+++ forms-example/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
+  genders = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
