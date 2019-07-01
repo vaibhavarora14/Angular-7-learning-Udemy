bdd5040336939198fd75756aae4b4b5e01e22c36
Vaibhav Arora <varora@catalystone.com>
2019-07-01 17:03:08 +0530

    -- submitting async validators

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArrayName, FormArray, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,21 @@ import { FormGroup, FormControl, FormArrayName, FormArray, Validators } from '@a
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  namesNotAllowed = ['Vaibhav', 'Maninder'];
+  emailsNotAllowed = ['vaibhav@test.com', 'maninder@test.com'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null),
-        email: new FormControl(null),
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)] ),
+        email: new FormControl(null, [Validators.required, Validators.email], [this.forbiddenEmails.bind(this)]),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([]),
     });
+    this.signupForm.get('userData.username').statusChanges.subscribe(
+      (value) => console.log(value)
+    );
   }
 
   submitDetails() {
@@ -29,4 +35,32 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (this.signupForm.get('hobbies') as FormArray).push(control);
   }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.namesNotAllowed.indexOf(control.value) !== -1) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl):
+    Promise<{[s: string]: boolean}> |
+    Observable<{[s: string]: boolean}> {
+      const promise = new Promise<{[s: string]: boolean}>((resolve, reject) => {
+        setTimeout(() => {
+          if (this.emailsNotAllowed.indexOf(control.value) !== -1) {
+            resolve({emailIsForbidden: true});
+          } else {
+            resolve(null);
+          }
+        }, 1500);
+      });
+
+      return promise;
+    }
+
+    getHobbies() {
+      const hobbies =  this.signupForm.get('hobbies') as FormArray;
+      return hobbies.controls;
+    }
 }
