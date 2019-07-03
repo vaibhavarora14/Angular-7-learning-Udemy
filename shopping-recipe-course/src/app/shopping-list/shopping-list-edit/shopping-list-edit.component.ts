5f0358a51cf689f16eb834854eac47f928aaaecb
Vaibhav Arora <varora@catalystone.com>
2019-07-03 15:31:08 +0530

    #6 Implemented forms knowledge in shopping list

--- shopping-recipe-course/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ shopping-recipe-course/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -3,51 +3,29 @@ import { Ingredient } from '../../shared/ingredient.model';
 import { stringify } from 'querystring';
 import { empty } from 'rxjs';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.scss']
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent {
 
   nameValue: string;
   amountValue: number;
-  addButtonState: boolean;
-  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
-  @ViewChild('addButton', { static: true }) addButtonRef: ElementRef;
 
-  constructor(private shoppingService: ShoppingListService) {
-    // this.validateForm();
-  }
+  constructor(private shoppingService: ShoppingListService) {}
 
-  ngOnInit(): void {
-    this.validateForm();
-  }
-
-  addIngredient(): void {
-    if (this.nameInputRef.nativeElement.value && this.amountInputRef.nativeElement.value) {
-      const ingredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+  addIngredient(form: NgForm): void {
+    if (form.valid) {
+      const ingredient = new Ingredient(form.value.name, form.value.amount);
       this.shoppingService.addIngredient(ingredient);
-      this.clearForm();
-    }
-    // const ingredient = new Ingredient(this.nameValue, this.amountValue);
-    // this.ingredient.emit(ingredient);
-    // this.clearForm();
-  }
-
-  validateForm() {
-    if (this.nameInputRef.nativeElement.value && this.amountInputRef.nativeElement.value) {
-      // this.addButtonRef.nativeElement.disabled = false;
-      this.addButtonState = false;
-    } else {
-      // this.addButtonRef.nativeElement.disabled = true;
-      this.addButtonState = true;
+      this.clearForm(form);
     }
   }
 
-  clearForm(): void {
-    this.nameInputRef.nativeElement.value = this.amountInputRef.nativeElement.value = undefined;
+  clearForm(form: NgForm): void {
+    form.reset();
   }
 }
