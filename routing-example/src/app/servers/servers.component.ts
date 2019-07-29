59a2aac30859852f0983bc7353160ae20825c2fb
Vaibhav Arora <varora@catalystone.com>
2019-07-29 10:44:14 +0530

    #8 - submitting programmatic navigation example

--- routing-example/src/app/servers/servers.component.ts
+++ routing-example/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    this.router.navigate(['servers']);
+
+    // example of relative path and error what is causes.
+    // this.router.navigate(['servers'], {relativeTo: this.activatedRoute});
   }
 
 }
