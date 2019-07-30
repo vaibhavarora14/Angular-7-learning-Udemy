fcf0af9a9d3f933294258f2a8289fc698de177cf
Vaibhav Arora <varora@catalystone.com>
2019-07-30 13:20:57 +0530

    #8 - submitting example of params in routing and it's observable

--- routing-example/src/app/users/user/user.component.ts
+++ routing-example/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramId = 'id';
+  paramName = 'name';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params[this.paramId],
+      name: this.route.snapshot.params[this.paramName],
+    };
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user = {
+          id: params[this.paramId],
+          name: params[this.paramName],
+        };
+      }
+    );
   }
 
 }
