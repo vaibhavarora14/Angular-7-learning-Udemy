2084826cf012a89b43010e822f1612cf50e84211
Vaibhav Arora <varora@catalystone.com>
2019-08-08 12:48:13 +0530

    #7: Submitting examples of subject

--- observables-example/src/app/app.component.ts
+++ observables-example/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'observables-example';
+  isActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+   this.userService.isActivated.subscribe(
+     (activated: boolean) => {
+       this.isActivated = activated;
+     }
+   );
+  }
 }
--- observables-example/src/app/user/user.component.ts
+++ observables-example/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +9,19 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   id: number;
+  isActivated = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
 
+  }
+
+  activate() {
+    this.isActivated = !this.isActivated;
+    this.userService.isActivated.next(this.isActivated);
   }
 }
